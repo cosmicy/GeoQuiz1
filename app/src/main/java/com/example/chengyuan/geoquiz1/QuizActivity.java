c55f9ccfe924615f2de8693203abd59995a780ef
cy <cosmicy@outlook.com>
2015-07-14 22:59:01 +0800

    完成2.10挑战练习三：从按钮到图标按钮

--- app/src/main/java/com/example/chengyuan/geoquiz1/QuizActivity.java
+++ app/src/main/java/com/example/chengyuan/geoquiz1/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +15,8 @@ public class QuizActivity extends ActionBarActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
-    private Button mPrevButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mTextView;
     private int mIndex = 0;
 
@@ -88,7 +89,7 @@ public class QuizActivity extends ActionBarActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,7 +98,7 @@ public class QuizActivity extends ActionBarActivity {
                 frash();
             }
         });
-        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
