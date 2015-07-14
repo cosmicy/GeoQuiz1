609014d12ea261981bdb9c43c0115eee990012ea
cosmicy <cosmicy@outlook.com>
2015-07-14 13:52:53 +0800

    init

---
+++ app/src/main/java/com/example/chengyuan/geoquiz1/QuizActivity.java
@@ -0,0 +1,107 @@
+package com.example.chengyuan.geoquiz1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class QuizActivity extends ActionBarActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mTextView;
+    private int mIndex = 0;
+
+    private TrueFalse[] arr = new TrueFalse[]{
+            new TrueFalse(R.string.q1, true),
+            new TrueFalse(R.string.q2, true),
+            new TrueFalse(R.string.q3, true),
+            new TrueFalse(R.string.q4, false),
+            new TrueFalse(R.string.q5, false),
+    };
+
+    private void frash() {
+        //mTextView = (TextView)findViewById(R.id.question_text_view);
+        mTextView.setText(arr[mIndex].getQuestion());
+    }
+
+    private void check(boolean userPressed) {
+        boolean answer = arr[mIndex].isTrueQuestion();
+        int message = 0;
+        if (userPressed == answer) {
+            message = R.string.correct_toast;
+        } else {
+            message = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTextView = (TextView)findViewById(R.id.question_text_view);
+
+        frash();
+
+        //true的方法
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                check(true);
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                check(false);
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                mIndex = (mIndex + 1) % arr.length;
+                frash();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
